415b613f578d501d5ed2f5878440d2a2a59140ef
shimakser <shimakser@gmail.com>
2021-09-02 19:45:07 +0300

    Created a project

---
+++ src/main/java/by/shimakser/ManagingAdApp.java
@@ -0,0 +1,11 @@
+package by.shimakser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagingAdApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ManagingAdApp.class, args);
+    }
+}
