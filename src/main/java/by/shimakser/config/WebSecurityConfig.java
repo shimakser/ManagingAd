1ae1dcb307fec461e441dc6a8066e8c640018f26
shimakser <shimakser@gmail.com>
2021-09-04 14:08:52 +0300

    Created a WebSecurityConfig to encode the password with BCryptPasswordEncoder

---
+++ src/main/java/by/shimakser/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package by.shimakser.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/managingad/**").permitAll()
+                .and()
+                .csrf().disable();
+    }
+}
