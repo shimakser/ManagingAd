76cb5f30a7c32af268764049514a23b06778aa4a
shimakser <shimakser@gmail.com>
2021-10-01 16:07:06 +0300

    Added new repository for Office-entity

---
+++ src/main/java/by/shimakser/repository/OfficeRepository.java
@@ -0,0 +1,7 @@
+package by.shimakser.repository;
+
+import by.shimakser.model.Office;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OfficeRepository extends JpaRepository<Office, Long> {
+}
