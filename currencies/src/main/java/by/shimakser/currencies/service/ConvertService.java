c005c0f7a6d5cd1a97578496cda412f56d2d0474
shimakser <shimakser@gmail.com>
2021-11-30 14:28:21 +0300

    Rewrote currencies/ConvertService

--- currencies/src/main/java/by/shimakser/currencies/service/ConvertService.java
+++ currencies/src/main/java/by/shimakser/currencies/service/ConvertService.java
@@ -1,42 +1,28 @@
 package by.shimakser.currencies.service;
 
+import by.shimakser.currencies.model.Currencies;
 import by.shimakser.currencies.model.Currency;
 import by.shimakser.currencies.model.CurrencyCode;
-import net.minidev.json.JSONObject;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @Service
 public class ConvertService {
 
-    public List<Currency> getListEntity(JSONObject json) {
-        Map<String, Map<String, JSONObject>> mapOfCurrencies = (LinkedHashMap) json.get("Valute");
-
-        List<Currency> listOfCurrency = new ArrayList<>();
-        Arrays.stream(CurrencyCode.values())
-                .map(code -> mapOfCurrencies.get(code.toString()))
-                .map(Map::values)
-                .map(this::convertToEntity)
-                .forEach(listOfCurrency::add);
-        return listOfCurrency;
+    public List<Currency> getListEntity(Currencies currencies) {
+        return Arrays.stream(CurrencyCode.values())
+                .map(code -> currencies.getValute().get(code.toString()))
+                .collect(Collectors.toList());
     }
 
-    public Currency getEntity(JSONObject json, String id) {
-        Map<String, Map<String, JSONObject>> mapOfCurrencies = (LinkedHashMap) json.get("Valute");
-        return mapOfCurrencies.values()
+    public Currency getEntity(Currencies currencies, String id) {
+        return currencies.getValute().values()
                 .stream()
-                .filter(stringJom -> stringJom.containsValue(id))
-                .map(Map::values)
-                .map(this::convertToEntity)
-                .collect(Collectors.toList())
-                .get(0);
-    }
-
-    public Currency convertToEntity(Collection<JSONObject> values) {
-        String[] value = values.toString().substring(1, values.toString().length() - 1).split(", ");
-
-        return new Currency(value[0], value[1], value[2], value[3], value[4], value[5], value[6]);
+                .filter(currency -> currency.getId().equals(id))
+                .findFirst().orElseThrow(() -> new NoSuchElementException("Incorrect id: " + id));
     }
 }
