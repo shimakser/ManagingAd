b72fc7a26b43da3dc24724abf217831938544790
shimakser <shimakser@gmail.com>
2021-12-01 10:23:56 +0300

    Fixed currency-output from currencies-service

--- dto/src/main/java/by/shimakser/dto/CurrencyDto.java
+++ dto/src/main/java/by/shimakser/dto/CurrencyDto.java
@@ -1,15 +1,23 @@
 package by.shimakser.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class CurrencyDto {
 
+    @JsonProperty(value = "ID")
     private String id;
+    @JsonProperty(value = "NumCode")
     private String numCode;
+    @JsonProperty(value = "CharCode")
     private String charCode;
+    @JsonProperty(value = "Nominal")
     private String nominal;
+    @JsonProperty(value = "Name")
     private String name;
+    @JsonProperty(value = "Value")
     private String value;
+    @JsonProperty(value = "Previous")
     private String previous;
 }
