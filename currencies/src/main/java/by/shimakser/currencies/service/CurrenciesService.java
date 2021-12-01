d106672e8f342b6f0edc059a799d632b319540c1
shimakser <shimakser@gmail.com>
2021-12-01 16:07:49 +0300

    Wrote checking currencies from db before calling external api

--- currencies/src/main/java/by/shimakser/currencies/service/CurrenciesService.java
+++ currencies/src/main/java/by/shimakser/currencies/service/CurrenciesService.java
@@ -2,11 +2,14 @@ package by.shimakser.currencies.service;
 
 import by.shimakser.currencies.feign.CurrenciesFeignClient;
 import by.shimakser.currencies.model.Currency;
+import by.shimakser.currencies.repository.CurrenciesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CurrenciesService {
@@ -15,15 +18,20 @@ public class CurrenciesService {
 
     private final ConvertService convertService;
 
+    private final CurrenciesRepository currenciesRepository;
+
     @Autowired
-    public CurrenciesService(CurrenciesFeignClient currenciesFeignClient, ConvertService convertService) {
+    public CurrenciesService(CurrenciesFeignClient currenciesFeignClient, ConvertService convertService, CurrenciesRepository currenciesRepository) {
         this.currenciesFeignClient = currenciesFeignClient;
         this.convertService = convertService;
+        this.currenciesRepository = currenciesRepository;
     }
 
     @Transactional
     public List<Currency> getCurrencies() {
-        return convertService.getListEntity(currenciesFeignClient.getCurrencies());
+        return  !currenciesRepository.findAll().isEmpty()
+                ? currenciesRepository.findAll()
+                : new ArrayList<>(currenciesFeignClient.getCurrencies().getValute().values());
     }
 
     @Transactional
