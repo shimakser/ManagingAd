16206bbca5922b297d41faa3d21e92befba387ad
shimakser <shimakser@gmail.com>
2021-12-01 15:32:25 +0300

    Wrote simple scheduler for the currencies

--- currencies/src/main/java/by/shimakser/currencies/Currencies.java
+++ currencies/src/main/java/by/shimakser/currencies/Currencies.java
@@ -2,7 +2,9 @@ package by.shimakser.currencies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class Currencies {
 
--- currencies/src/main/java/by/shimakser/currencies/model/Currency.java
+++ currencies/src/main/java/by/shimakser/currencies/model/Currency.java
@@ -4,24 +4,47 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Document(collection = "Currencies")
 public class Currency {
 
+    @MongoId
     @JsonProperty(value = "ID")
     private String id;
+
+    @Field(value = "num_code")
     @JsonProperty(value = "NumCode")
     private String numCode;
+
+    @Field(value = "char_code")
     @JsonProperty(value = "CharCode")
     private String charCode;
+
+    @Field(value = "nominal")
     @JsonProperty(value = "Nominal")
     private String nominal;
+
+    @Field(value = "name")
     @JsonProperty(value = "Name")
     private String name;
+
+    @Field(value = "value")
     @JsonProperty(value = "Value")
     private String value;
+
+    @Field(value = "previous")
     @JsonProperty(value = "Previous")
     private String previous;
+
+    @Field(value = "upd_date")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private LocalDateTime updDate;
 }
