d7c0b271ad4c72968fc6deb2d889d298aa1941ae
shimakser <shimakser@gmail.com>
2021-12-06 13:49:36 +0300

    Changed Office-entity for the export from .csv

--- application/src/main/java/by/shimakser/model/office/Office.java
+++ application/src/main/java/by/shimakser/model/office/Office.java
@@ -1,6 +1,10 @@
 package by.shimakser.model.office;
 
 import by.shimakser.converter.JSONObjectConverter;
+import by.shimakser.converter.JsonConverter;
+import by.shimakser.converter.ListConverter;
+import com.opencsv.bean.CsvBindByName;
+import com.opencsv.bean.CsvCustomBindByName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,13 +21,24 @@ import java.util.List;
 public class Office {
 
     @Id
+    @CsvBindByName(column = "id")
     private Long id;
+
+    @CsvBindByName(column = "office_title")
     private String officeTitle;
+
+    @CsvBindByName(column = "office_address")
     private String officeAddress;
+
+    @CsvBindByName(column = "office_price")
     private Double officePrice;
+
+    @CsvCustomBindByName(column = "office_contacts", converter = ListConverter.class)
     @OneToMany(fetch = FetchType.EAGER)
     @JoinColumn(name = "office_id")
     private List<Contact> officeContacts;
+
+    @CsvCustomBindByName(column = "office_description", converter = JsonConverter.class)
     @Convert(converter = JSONObjectConverter.class)
     private JSONObject officeDescription;
 
